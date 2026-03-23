85d575fbbb4e63fa79c8e973a4f004037a35c163
esalkic1 <esalkic1@etf.unsa.ba>
2026-03-23 18:00:57 +0100

    Initial commit

---
+++ src/main/java/ba/unsa/etf/NBP/NbpApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.NBP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ba/unsa/etf/NBP/NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.NBP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
