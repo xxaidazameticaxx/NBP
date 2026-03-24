8b956b36b14d3b87e15455d5a3e6294212a4696b
esalkic1 <esalkic1@etf.unsa.ba>
2026-03-24 11:16:43 +0100

    Add swagger support

---
+++ src/main/java/ba/unsa/etf/NBP/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.NBP.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        // ALLOW SWAGGER ENDPOINTS
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+
+                        .anyRequest().permitAll() // needs to change when we create roles
+                );
+
+        return http.build();
+    }
+}
